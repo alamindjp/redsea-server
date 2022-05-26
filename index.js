e40a4f430e3140d732caeaff8c3f2e0295f2c740
alamindjp <alaminhossaindjp@gmail.com>
2022-05-27 03:21:59 +0600

    create app for get admin for dashboard all user page requiring

--- index.js
+++ index.js
@@ -106,6 +106,14 @@ async function run() {
                 res.status(403).send({ message: 'Forbidden' });
             }
         });
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({email: email})
+            const isAdmin = user.role === 'admin';
+            res.send(isAdmin)
+        });
+
+
     }
     finally {
 
